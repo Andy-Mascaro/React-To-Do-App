94b0fdbe1303c08390f79cf423363e36ae8b6cc9
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-03-30 17:13:58 -0700

    added userjs

---
+++ src/services/users.js
@@ -0,0 +1,26 @@
+import { client, checkError } from './client';
+
+export default function users() {
+  return client.auth.session() && client.auth.session().user.email;
+}
+
+export async function singUpUser(email, password) {
+  const { user, error } = await client.auth.signUpUser({ email, password });
+  if (error) {
+    throw error;
+  }
+  return user;
+}
+
+export async function signInUser(email, password) {
+  const { user, error } = await client.auth.signInUser({ email, password });
+  if (error) {
+    throw error;
+  }
+  return user;
+}
+
+export async function logout() {
+  const resp = await client.auth.signOut();
+  return checkError(resp);
+}
